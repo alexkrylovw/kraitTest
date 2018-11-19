a82d9fd23ea9168bea3e506bad1ff51f64112ee3
alexkrylovw <alexkrylovw@gmail.com>
2018-11-19 23:50:48 +0300

    pricing section added(with price div's issue)

---
+++ js/scripts.js
@@ -0,0 +1,119 @@
+///////////////////JQuery mouseovering funcs
+////pricings
+///price-block-1
+//in
+$("#price-block-1").mouseover(function(){
+    $("#price-block-1").css("background", "#fff");
+    $("#price-block-1").css("-webkit-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-1").css("-moz-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-1").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-1").css("color", "#fff");
+    $("#price-button-1").css("border-width", "3px");
+    $("#price-shape-1-1").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-shape-2-1").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-button-1").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-button-1").css("color", "#fff");
+    $("#price-list-1").css("color","#777");
+
+});
+//out
+$("#price-block-1").mouseout(function(){
+    $("#price-block-1").css("background", "#f7fafc");
+    $("#price-block-1").css("-webkit-box-shadow", "none");
+    $("#price-block-1").css("-moz-box-shadow", "none");
+    $("#price-block-1").css("box-shadow", "none");
+    $("#price-1").css("color", "#222");
+    $("#price-button-1").css("border-width", "1px");
+    $("#price-shape-1-1").css("background", "#fff");
+    $("#price-button-1").css("background", "#fff");
+    $("#price-button-1").css("color", "#222");
+    $("#price-list-1").css("color","#828282");
+});
+///price-block-2
+//in
+$("#price-block-2").mouseover(function(){
+    $("#price-block-2").css("background", "#fff");
+    $("#price-block-2").css("-webkit-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-2").css("-moz-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-2").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-2").css("color", "#fff");
+    $("#price-button-2").css("border-width", "3px");
+    $("#price-shape-1-2").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-shape-2-2").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-button-2").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-button-2").css("color", "#fff");
+    $("#price-list-1").css("color","#777");
+   
+});
+//out
+$("#price-block-2").mouseout(function(){
+    $("#price-block-2").css("background", "#f7fafc");
+    $("#price-block-2").css("-webkit-box-shadow", "none");
+    $("#price-block-2").css("-moz-box-shadow", "none");
+    $("#price-block-2").css("box-shadow", "none");
+    $("#price-2").css("color", "#222");
+    $("#price-button-2").css("border-width", "1px");
+    $("#price-shape-1-2").css("background", "#fff");
+    $("#price-button-2").css("background", "#fff");
+    $("#price-button-2").css("color", "#222");
+    $("#price-list-1").css("color","#828282");
+});
+///price-block-3
+//in
+$("#price-block-3").mouseover(function(){
+    $("#price-block-3").css("background", "#fff");
+    $("#price-block-3").css("-webkit-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-3").css("-moz-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-3").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-3").css("color", "#fff");
+    $("#price-button-3").css("border-width", "3px");
+    $("#price-shape-1-3").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-shape-2-3").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-button-3").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-button-3").css("color", "#fff");
+    $("#price-list-1").css("color","#777");
+   
+});
+//out
+$("#price-block-3").mouseout(function(){
+    $("#price-block-3").css("background", "#f7fafc");
+    $("#price-block-3").css("-webkit-box-shadow", "none");
+    $("#price-block-3").css("-moz-box-shadow", "none");
+    $("#price-block-3").css("box-shadow", "none");
+    $("#price-3").css("color", "#222");
+    $("#price-button-3").css("border-width", "1px");
+    $("#price-shape-1-3").css("background", "#fff");
+    $("#price-button-3").css("background", "#fff");
+    $("#price-button-3").css("color", "#222");
+    $("#price-list-1").css("color","#828282");
+});
+///price-block-4
+//in
+$("#price-block-4").mouseover(function(){
+    $("#price-block-4").css("background", "#fff");
+    $("#price-block-4").css("-webkit-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-4").css("-moz-box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-block-4").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
+    $("#price-4").css("color", "#fff");
+    $("#price-button-4").css("border-width", "3px");
+    $("#price-shape-1-4").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-shape-2-4").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-button-4").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
+    $("#price-button-4").css("color", "#fff");
+    $("#price-list-1").css("color","#777");
+   
+});
+//out
+$("#price-block-4").mouseout(function(){
+    $("#price-block-4").css("background", "#f7fafc");
+    $("#price-block-4").css("-webkit-box-shadow", "none");
+    $("#price-block-4").css("-moz-box-shadow", "none");
+    $("#price-block-4").css("box-shadow", "none");
+    $("#price-4").css("color", "#222");
+    $("#price-button-4").css("border-width", "1px");
+    $("#price-shape-1-4").css("background", "#fff");
+    $("#price-button-4").css("background", "#fff");
+    $("#price-button-4").css("color", "#222");
+    $("#price-list-1").css("color","#828282");
+});
+/////////////////////////////////////////////////////////
