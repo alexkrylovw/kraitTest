c329c9234565955d52e19cdb31566f1caf51e0e2
alexkrylovw <alexkrylovw@gmail.com>
2018-11-20 21:54:03 +0300

    Pricings and download sections fully added and customized with js

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,7 @@ $("#price-block-1").mouseover(function(){
     $("#price-block-1").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
     $("#price-1").css("color", "#fff");
     $("#price-button-1").css("border-width", "3px");
-    $("#price-shape-1-1").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
-    $("#price-shape-2-1").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-shape-1").css("background", "url('../img/backgrounds/price-background-grad.svg')");
     $("#price-button-1").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
     $("#price-button-1").css("color", "#fff");
     $("#price-list-1").css("color","#777");
@@ -24,7 +23,7 @@ $("#price-block-1").mouseout(function(){
     $("#price-block-1").css("box-shadow", "none");
     $("#price-1").css("color", "#222");
     $("#price-button-1").css("border-width", "1px");
-    $("#price-shape-1-1").css("background", "#fff");
+    $("#price-shape-1").css("background", "url('../img/backgrounds/price-background.svg')");
     $("#price-button-1").css("background", "#fff");
     $("#price-button-1").css("color", "#222");
     $("#price-list-1").css("color","#828282");
@@ -38,8 +37,7 @@ $("#price-block-2").mouseover(function(){
     $("#price-block-2").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
     $("#price-2").css("color", "#fff");
     $("#price-button-2").css("border-width", "3px");
-    $("#price-shape-1-2").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
-    $("#price-shape-2-2").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-shape-2").css("background", "url('../img/backgrounds/price-background-grad.svg')");
     $("#price-button-2").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
     $("#price-button-2").css("color", "#fff");
     $("#price-list-1").css("color","#777");
@@ -53,7 +51,7 @@ $("#price-block-2").mouseout(function(){
     $("#price-block-2").css("box-shadow", "none");
     $("#price-2").css("color", "#222");
     $("#price-button-2").css("border-width", "1px");
-    $("#price-shape-1-2").css("background", "#fff");
+    $("#price-shape-2").css("background", "url('../img/backgrounds/price-background.svg')");
     $("#price-button-2").css("background", "#fff");
     $("#price-button-2").css("color", "#222");
     $("#price-list-1").css("color","#828282");
@@ -67,8 +65,7 @@ $("#price-block-3").mouseover(function(){
     $("#price-block-3").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
     $("#price-3").css("color", "#fff");
     $("#price-button-3").css("border-width", "3px");
-    $("#price-shape-1-3").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
-    $("#price-shape-2-3").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-shape-3").css("background", "url('../img/backgrounds/price-background-grad.svg')");
     $("#price-button-3").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
     $("#price-button-3").css("color", "#fff");
     $("#price-list-1").css("color","#777");
@@ -82,7 +79,7 @@ $("#price-block-3").mouseout(function(){
     $("#price-block-3").css("box-shadow", "none");
     $("#price-3").css("color", "#222");
     $("#price-button-3").css("border-width", "1px");
-    $("#price-shape-1-3").css("background", "#fff");
+    $("#price-shape-3").css("background", "url('../img/backgrounds/price-background.svg')");
     $("#price-button-3").css("background", "#fff");
     $("#price-button-3").css("color", "#222");
     $("#price-list-1").css("color","#828282");
@@ -96,8 +93,7 @@ $("#price-block-4").mouseover(function(){
     $("#price-block-4").css("box-shadow", "0px 3px 27px -4px rgba(0,0,0,0.75)");
     $("#price-4").css("color", "#fff");
     $("#price-button-4").css("border-width", "3px");
-    $("#price-shape-1-4").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
-    $("#price-shape-2-4").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)"); /////???
+    $("#price-shape-4").css("background", "url('../img/backgrounds/price-background-grad.svg')");
     $("#price-button-4").css("background", "linear-gradient(to right, #fd712c 0%,#f31e7a 100%)");
     $("#price-button-4").css("color", "#fff");
     $("#price-list-1").css("color","#777");
@@ -111,9 +107,39 @@ $("#price-block-4").mouseout(function(){
     $("#price-block-4").css("box-shadow", "none");
     $("#price-4").css("color", "#222");
     $("#price-button-4").css("border-width", "1px");
-    $("#price-shape-1-4").css("background", "#fff");
+    $("#price-shape-4").css("background", "url('../img/backgrounds/price-background.svg')");
     $("#price-button-4").css("background", "#fff");
     $("#price-button-4").css("color", "#222");
     $("#price-list-1").css("color","#828282");
 });
 /////////////////////////////////////////////////////////
+
+////download buttons
+///1st
+//in
+$("#download-item-1").mouseover(function(){
+    $("#download-image-1").attr("src", "/img/icons/device-icons/apple_i_g.png");
+});
+
+//out
+$("#download-item-1").mouseout(function(){
+    $("#download-image-1").attr("src", "/img/icons/device-icons/apple_i.png");
+});
+///2nd
+//in
+$("#download-item-2").mouseover(function(){
+    $("#download-image-2").attr("src", "/img/icons/device-icons/android_i_g.png");
+});
+//out
+$("#download-item-2").mouseout(function(){
+    $("#download-image-2").attr("src", "/img/icons/device-icons/android_i.png");
+});
+///3rd
+//in
+$("#download-item-3").mouseover(function(){
+    $("#download-image-3").attr("src", "/img/icons/device-icons/windows_i_g.png");
+});
+//out
+$("#download-item-3").mouseout(function(){
+    $("#download-image-3").attr("src", "/img/icons/device-icons/windows_i.png");
+});
